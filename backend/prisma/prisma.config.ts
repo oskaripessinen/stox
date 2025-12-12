2ac7f300223fa9fca71f5e5705d715da6dd7654a
oskaripessinen <oskari.pessinen@gmail.com>
2025-12-12 18:16:04 +0200

    db connection working

--- backend/prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import { defineConfig } from "prisma/config";
-import { config } from "dotenv";
-
-config({ path: path.resolve(__dirname, "../.env") });
-
-export default defineConfig({
-  schema: path.resolve(__dirname, "schema.prisma"),
-});
